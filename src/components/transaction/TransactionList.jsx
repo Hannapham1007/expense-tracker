c4c70f7e556d643b4a8e9b6e936df54eba599218
Hannapham1007 <hannaphamm1007@gmail.com>
2024-08-18 11:37:28 +0200

    added filter, search transaction for guest user

--- src/components/transaction/TransactionList.jsx
+++ src/components/transaction/TransactionList.jsx
@@ -1,9 +1,13 @@
 import { useContext, useState, useEffect } from "react";
 import IncomeList from "../income/IncomeList";
 import ExpenseList from "../expense/ExpenseList";
-import { ExpenseContext, IncomeContext } from "../../App";
+import { ExpenseContext, IncomeContext, UserContext } from "../../App";
 import SearchBar from "../filter/SearchBar";
 import DateRangeFilter from "../filter/DateRangeFilter";
+import { useSelector } from "react-redux";
+import { selectExpenses } from "../../reducers/expense";
+import { selectIncomes } from "../../reducers/income";
+import { selectCategories } from "../../reducers/category";
 
 function TransactionList() {
   const { incomes } = useContext(IncomeContext);
@@ -11,6 +15,16 @@ function TransactionList() {
   const [filterText, setFilterText] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const {loggedInUser} = useContext(UserContext);
+  const expenseSelector = useSelector(selectExpenses);
+  const incomeSelector = useSelector(selectIncomes);
+  const categorySelector = useSelector(selectCategories);
+
+  const getCategoryNameById = (id) =>{
+    const category = categorySelector.find((cat) => String(cat.id) === String(id));
+    return category ? category.name : "";
+  }
+
 
   useEffect(() => {
     defaultCurrentMonth();
@@ -59,28 +73,42 @@ function TransactionList() {
       end.setHours(0, 0, 0, 0);
 
       return (
-        (isMatchedCategory || isMatchedDescription) &&
-        (!startDate ||
-          !endDate ||
-          (transactionDate >= start && transactionDate <= end))
+        (isMatchedCategory || isMatchedDescription) && (!startDate || !endDate || (transactionDate >= start && transactionDate <= end))
       );
     });
   };
 
-  const filteredIncome = filteredList(incomes);
-  const filteredExpense = filteredList(expenses);
+  const filteredListForGuestUser = (list) =>{
+    const textInput = filterText.toLowerCase();
+    return list.filter((item) => {
+      const isMatchedCategory = getCategoryNameById(item.category).toLowerCase().includes(textInput);
+      const isMatchedDescription = item.description.toLowerCase().includes(textInput);
+
+      const transactionDate = new Date(item.date);
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+      start.setHours(0, 0, 0, 0);
+      end.setHours(0, 0, 0, 0);
+
+      return (
+        (isMatchedCategory || isMatchedDescription) && (!startDate || !endDate || (transactionDate >= start && transactionDate <= end))
+      );
+
+    })
+  }
+  
+  const filteredIncome = loggedInUser ? filteredList(incomes) : filteredListForGuestUser(incomeSelector);
+  const filteredExpense = loggedInUser ? filteredList(expenses) : filteredListForGuestUser(expenseSelector);
 
   return (
     <div className="">
         <h2 className="text-center text-uppercase fw-bold">My Transactions</h2>
         <div className="mb-3 mx-3">
-
         <SearchBar
           filterText={filterText}
           setFilterText={setFilterText}
         ></SearchBar>
       </div>
-
       <DateRangeFilter
         startDate={startDate}
         setStartDate={setStartDate}
